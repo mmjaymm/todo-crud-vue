5296669c6bc7c090510ad7cf679ebbc267c93379
Mark Jay <mjay@gmail.com>
2023-06-13 09:00:20 +0800

    create migration table

---
+++ database/migrations/2023_06_13_005932_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject')->nullable(false);
+            $table->string('task')->nullable(false);
+            $table->text('descriptions');
+            $table->string('attachment');
+            $table->enum('status', ['new', 'inprogress', 'completed'])->nullable(false);
+            $table->softDeletesTz();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
